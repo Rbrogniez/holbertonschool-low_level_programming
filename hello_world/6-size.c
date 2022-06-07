4fcaac26ad13a6c888c63168833ed674e042c02f
Rbrogniez <5013@holbertonschool.com>
2022-06-07 16:21:01 +0200

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,9 +10,10 @@ char charType;
 int intType;
 long int longintType;
 float floatType;
-printf("Size of a char: %z bytes\n", sizeof(charType));
-printf("Size of a int: %z bytes\n", sizeof(intType));
-printf("Size of a long int: %z bytes\n", sizeof(longintType));
-printf("Size of a float: %z bytes\n", sizeof(floatType));
+printf("Size of a char: %z byte(s)\n", sizeof(charType));
+printf("Size of a int: %z byte(s)\n", sizeof(intType));
+printf("Size of a long int: %z byte(s)\n", sizeof(longintType));
+printf("Size of a long long in: %z byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %z byte(s)\n", sizeof(floatType));
 return (0);
 }
