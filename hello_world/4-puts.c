4101857c5ba8f615e7650a0b4ce6d3b26186ba68
Rbrogniez <5013@holbertonschool.com>
2022-06-07 10:59:14 +0200

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#!/bin/bash
-#include<stdio.h>
-int main() {
-   puts('"Programming is like building a multilingual puzzle');
-   return 0;
+#inculde<stdio.h>
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+return(0);
 }
