c422fa9d1342209e19ba6fc7bfa4bbba46c0ae11
Rbrogniez <5013@holbertonschool.com>
2022-06-08 17:52:55 +0200

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-/* more headers goes there */
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main()
+ *
+ *
+ *
+ */
+int main(void)
 {
-  int ch;
-  for(ch='a';ch<='z';ch++)
-    putchar(ch);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
