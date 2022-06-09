3a1b5215fba044e6dbd24dc3acbda2d854593f64
Rbrogniez <5013@holbertonschool.com>
2022-06-09 10:06:50 +0200

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-char ch;
+  int ch;
 
-for (ch = 'a'; ch <= 'Z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
+for (ch= 'a'; ch<= 'z'; ch++)
+putchar (ch);
+for (ch= 'A'; ch<= 'Z'; ch++)
+putchar (ch);
+ putchar ('\n');
 
-return (0);
-
+   return (0);
+   
 }
