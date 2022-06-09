4cb6970b9df54885c4d7a38c1705940930c5aaa5
Rbrogniez <5013@holbertonschool.com>
2022-06-09 10:40:13 +0200

    8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= 'f'; n++)
+
+putchar (n);
+
+putchar ('\n');
+
+return (0);
+}
---
+++ variables_if_else_while/8-print_base16.c~
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+
+putchar (n);
+
+putchar ('\n');
+
+return (0);
+}
