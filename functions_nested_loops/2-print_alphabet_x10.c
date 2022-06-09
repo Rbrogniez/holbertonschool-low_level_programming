1d4df33b3f581117114f817d4447343a11156e95
Rbrogniez <5013@holbertonschool.com>
2022-06-09 16:25:09 +0200

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,15 @@ int print_alphabet_x10(void)
 char ch;
 int x;
 for (x = 0 ; x < 10 ; x++)
+
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
+putchar('\n'); 
 }
-putchar('\n');
+
 
 return (0);
 
