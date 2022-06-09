6a83fa0ed365adf571b726cb4036aa7b6719567d
Rbrogniez <5013@holbertonschool.com>
2022-06-09 14:36:52 +0200

    0-putchar.c betty fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 int main(void)
 {
-  char str[8]="_putchar";
+char str[8] = "_putchar";
 int x;
- for (x = 0;str[x] != '\0' ;x++)
-    {
-      _putchar(str[x]);
-    }
-  _putchar ('\n');
+for (x = 0 ; str[x] != '\0' ; x++)
+{
+_putchar(str[x]);
+}
+_putchar ('\n');
 
-  return(0);
+return (0);
 
 }
