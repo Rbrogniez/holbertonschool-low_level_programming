e86939d4041292fbf52b27eae8dc73a8be832657
Rbrogniez <5013@holbertonschool.com>
2022-06-09 16:11:33 +0200

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int print_alphabet_x10(void)
+{
+char ch;
+int x;
+for (x = 0 ; x < 10 ; x++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+}
+putchar('\n');
+
+return (0);
+
+}
