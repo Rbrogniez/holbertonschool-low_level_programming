f30076e144e99c93acf5f95caf4330ed02aebe25
Rbrogniez <5013@holbertonschool.com>
2022-06-09 16:33:25 +0200

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /* more headers goes there */
 /**
-* main - Entry point
+* print_alphabet_x10 - Entry point
 *
 * Return: Always 0 (Success)
 */
 int print_alphabet_x10(void)
 {
-char ch;
-int x;
-for (x = 0 ; x < 10 ; x++)
+	char ch;
+	int x;
 
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-}
+	for (x = 0 ; x < 10 ; x++)
 
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
 
-return (0);
+	return (0);
 
 }
