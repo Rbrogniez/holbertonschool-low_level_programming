f51c0c0ae7b45f0c50a332b354b7a0942f326b00
Rbrogniez <5013@holbertonschool.com>
2022-06-10 10:34:18 +0200

    3-islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	return 1;
+		return (1);
 	else
-	return 0;
+		return (0);
 }
