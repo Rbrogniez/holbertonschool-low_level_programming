837644cc22c1729088f90b35ee355e8ff2339a71
Rbrogniez <5013@holbertonschool.com>
2022-06-13 17:53:07 +0200

    6-lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+void print_line(int n)
+
+{
+	int line;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+
+	for (line = 0; line < n; line++)
+	{
+	_putchar ('_');
+	}
+		_putchar ('\n');
+}
