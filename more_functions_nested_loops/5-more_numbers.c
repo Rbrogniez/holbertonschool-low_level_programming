e10cdcdbb1c662d7b6cc9f45602b7ab8dc0355db
Rbrogniez <5013@holbertonschool.com>
2022-06-13 16:24:43 +0200

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *
+ *Return: nothing.
+ */
+
+void more_numbers(void)
+
+{
+	int l;
+	for (l = 0; l<= 9; l++)
+	{
+		int n;
+
+			for (n = 0; n <= 14; n++)
+			{
+				if (n > 9 )
+				{
+				_putchar (n / 10 + '0');
+				_putchar (n % 10 + '0');
+				}
+	 			else
+				{
+				_putchar (n % 10 + '0');
+				}
+
+			}
+			_putchar ('\n');
+	}
+
+}
