cf17c28912ef03ed6c455179e75695614fe690d9
Rbrogniez <5013@holbertonschool.com>
2022-06-13 10:37:34 +0200

    0-isuper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *int _isupper - checks for uppercase character
+ *@c :
+ *
+ **/
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
