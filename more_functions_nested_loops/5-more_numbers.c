0703b0c026b4a96a0dfad95a7ec08218e89905c9
Rbrogniez <5013@holbertonschool.com>
2022-06-13 16:42:27 +0200

    5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@ void more_numbers(void)
 
 {
 	int l;
-	for (l = 0; l<= 9; l++)
+	for (l = 0; l <= 9; l++)
 	{
 		int n;
 
 			for (n = 0; n <= 14; n++)
 			{
-				if (n > 9 )
+				if (n > 9)
 				{
 				_putchar (n / 10 + '0');
 				}
