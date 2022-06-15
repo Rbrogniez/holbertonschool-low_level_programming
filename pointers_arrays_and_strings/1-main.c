d3b7828235ff029c85a4cf4240cd318d1097d727
Rbrogniez <5013@holbertonschool.com>
2022-06-15 14:27:59 +0200

    0-98

---
+++ pointers_arrays_and_strings/1-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	printf("Adresse of variable 'c': %p\n", &c);
+	printf("Adresse of variable 'n': %p\n", &n);
+	return(0);
+}
