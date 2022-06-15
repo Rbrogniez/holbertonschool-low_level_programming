11f447bb72a87e449c744744aac1bf35c7674e2d
Rbrogniez <5013@holbertonschool.com>
2022-06-15 16:12:40 +0200

    1-swap

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -1,15 +1,20 @@
+#include "main.h"
 #include <stdio.h>
-/**
- *
- *
- */
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char c;
-	int n;
+    int a;
+    int b;
 
-	printf("Adresse of variable 'c': %p\n", &c);
-	printf("Adresse of variable 'n': %p\n", &n);
-	return(0);
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
 }
