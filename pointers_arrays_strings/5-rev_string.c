c54b5a4633e58596dff78ae3250da53a8c98d93c
Rbrogniez <5013@holbertonschool.com>
2022-06-17 16:43:27 +0200

    5-rev_string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,27 @@
 #include "main.h"
 /**
- * rev_string - reverse string
+ * rev_string - reverses a string
  * @s: string
  */
-
 void rev_string(char *s)
-
 {
-	char temp;
-	int c,i,j,n;
-	c = 0;
-	while (s[c] != '\0')
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		c++;
+		len++;
 	}
 
-	for (i = 0, j = n - 1; i < j; ++i, --j);
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		temp = s[j];
-		s[j] = s[i];
-		s[i] = temp;
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
-
