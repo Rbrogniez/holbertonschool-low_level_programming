cca67931ad993782dc087adf294a62e5680f9ac7
Rbrogniez <5013@holbertonschool.com>
2022-06-17 14:44:42 +0200

    3-strcmp

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
  * _strncpy - Copy the second string to the first with only n char
- *
  * @dest: The first string
  * @src: The second string
- * @n: The number of char copied
- *
- * Return: The copied string dest
+ * @n:The number of char copied
+ * @return The copied string dest
  */
 
 char *_strncat(char *dest, char *src, int n)
