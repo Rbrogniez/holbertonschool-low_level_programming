62d9eb2328592f0e5ed4a0404acf170106518f45
Rbrogniez <5013@holbertonschool.com>
2022-06-17 14:12:02 +0200

    2-strncpy

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,9 @@
 #include "main.h"
-/**
-*_strncat - function
-*@dest: destination string
-*@src: dest string
-*@n: number of bytes
-*Return: value of dest
+/**_strncpy - function that copies a string
+*@dest: receive string copy of src
+*@src: sting copy in dest
+*@n: copy n bytes of src
+*Return: a pointer of variable dest string
 */
 
 char *_strncat(char *dest, char *src, int n)
