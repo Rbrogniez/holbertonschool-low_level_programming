bb8ad3cda51ed76505801dd00a35120e95bc137b
Rbrogniez <5013@holbertonschool.com>
2022-06-20 11:37:39 +0200

    3-factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,22 +2,28 @@
 /**
  * factorial - returns the factorial of a given number.
  *@n: print number
+*Return: result
  */
 
 int factorial(int n)
 
 {
-if (n == 0)
 
+	if (n == 0)
+		{
+		return (1);
+		}
+
+if (n < 0)
 	{
-		return 1;
+	return (-1);
 	}
 
-else
+	else
+		{
+		return (n * factorial(n - 1));
+		}
 
-	{
-		return(n * factorial(n-1));
-	}
 }
 
 
