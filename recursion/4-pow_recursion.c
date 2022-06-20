98444cb9d830afc91c7f7e35dfdea94053ec37eb
Rbrogniez <5013@holbertonschool.com>
2022-06-20 12:03:07 +0200

    4-pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ *@y:
+ *@x:
+*Return: result
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+
+	if (y == 0)
+	return (1);
+	if (y < 0)
+	return (-1);
+	return (_pow_recursion(x, y - 1) * x);
+
+}
