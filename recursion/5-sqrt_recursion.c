f573fc181678bc12c9aacb718dfc37b13f5d81dd
Rbrogniez <5013@holbertonschool.com>
2022-06-21 10:26:27 +0200

    5-sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,14 +5,25 @@
 *Return:
  */
 
+int squareroot(int n, int x)
+
+{
+	if (n == x*x)
+	return (x);
+	return (squareroot(n,x+1));
+}
 int _sqrt_recursion(int n)
 
 {
+	int x = 0;
 
-
-
-
-
+	if (n < 0)
+	return (-1);
+	if (n == 1)
+	return (1);
+	return (squareroot(n, x));
 }
+
+
 
 
