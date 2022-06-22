bbed545725ee0f1550b90ae1bc8eb5cea1506578
Rbrogniez <5013@holbertonschool.com>
2022-06-22 16:23:10 +0200

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,6 +10,21 @@
  */
 
 int main(int argc, char **argv)
+{
 
+	if (argc == 3)
 
+	{
+	int numb1 = atoi(argv[1]);
+	int numb2 = atoi(argv[2]);
 
+	printf("%d\n", numb1 * numb2);
+
+	return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
