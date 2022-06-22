aa8e05bb7f7562aaba4500256ea5fab9635f2dac
Rbrogniez <5013@holbertonschool.com>
2022-06-22 16:17:03 +0200

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument value
+ * Return: success
+ */
+
+int main(int argc, char **argv)
+
+
+
