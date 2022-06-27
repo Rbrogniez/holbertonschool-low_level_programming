11dc6a87b609b5d717419ba9a99e43c4362ba818
Rbrogniez <5013@holbertonschool.com>
2022-06-27 18:21:45 +0200

    2-calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *create_array - creates an array of chars, and initializes it with a
+ *specific char.
+ *@size: unsigned int
+ *@c: char
+ *Return: Tab
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+
+char *tab = malloc(size * sizeof(char));
+unsigned int i = 0;
+
+
+
+for (i = 0; i < size; i++)
+{
+	tab[i] = nmemb;
+}
+if (size == 0)
+{
+return (NULL);
+}
+
+
+return (tab);
+
+}
