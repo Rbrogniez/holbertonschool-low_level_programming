fc363e971826bdfc7411ebbe9badd54eef511877
Rbrogniez <5013@holbertonschool.com>
2022-06-29 12:50:34 +0200

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 
 {
     if (d == NULL)
-    printf("%%");
+    return;
 
     if ((*d).name == NULL && (*d).owner == NULL)
     printf("Name: (nil)\nAge: %.6f\nOwner: (nil)\n", (*d).age);
