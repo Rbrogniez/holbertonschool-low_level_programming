807267076db2cc040f6966d0a26b2f4a67431201
Rbrogniez <5013@holbertonschool.com>
2022-06-29 10:47:10 +0200

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG
 #define DOG
-
+/**
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
