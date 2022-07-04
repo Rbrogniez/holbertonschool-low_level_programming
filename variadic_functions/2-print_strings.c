492dba9bad28a8aa42b19684d88194b67ae56d00
Rbrogniez <5013@holbertonschool.com>
2022-07-04 16:14:29 +0200

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,11 +18,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n ; i++)
 	{
+		if (args == NULL)
+
+		{
+			printf ("(nil)");
+		}
+
 		printf("%s", va_arg(args, const char*));
 		if (i < n - 1 && separator != NULL)
 		{
 		printf("%s", separator);
 		}
+
 
 	}
 	printf("\n");
