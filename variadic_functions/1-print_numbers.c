6d19d5decc06ce794cd57d926ab281b8a8e07972
Rbrogniez <5013@holbertonschool.com>
2022-07-04 15:01:15 +0200

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,9 +3,8 @@
 #include "variadic_functions.h"
 /**
  * print_numbers - prints numbers, followed by a new line.
- * @separator:
- * @n:
- * @param:
+ * @separator: separator of intreger
+ * @n: number of intreger
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -17,19 +16,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-	{
-		printf("Nothing");
-	}
-
 
 	for (i = 0; i < n ; i++)
 	{
-		printf("%d",va_arg(args,const unsigned int));
-		if (i < n - 1)
+		printf("%d", va_arg(args, const unsigned int));
+		if (i < n - 1 && separator != NULL)
 		{
-		printf("%s",separator);
+		printf("%s", separator);
 		}
+
 	}
 	printf("\n");
 	va_end(args);
