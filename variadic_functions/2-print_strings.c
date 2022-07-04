365b01aa1df39b2882459cfad83e8d16a2d2d9f9
Rbrogniez <5013@holbertonschool.com>
2022-07-04 15:32:13 +0200

    2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: separator of intreger
+ * @n: number of intreger
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+
+	for (i = 0; i < n ; i++)
+	{
+		printf("%s", va_arg(args, const char*));
+		if (i < n - 1 && separator != NULL)
+		{
+		printf("%s", separator);
+		}
+		
+	}
+	printf("\n");
+	va_end(args);
+}
+
