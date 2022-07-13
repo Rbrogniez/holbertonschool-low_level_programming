efb7a399ecb0c96ad1728ef23324039e2dacb85c
Rbrogniez <5013@holbertonschool.com>
2022-07-13 13:56:51 +0200

    fix betty AGAIN

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,16 +12,10 @@ size_t print_list(const list_t *h)
 	size_t index = 0;
 
 	for (; h != NULL; h = h->next, index++)
-
 	if (h->str == NULL)
-	{
 		printf("[0] (nil)\n");
-	}
-
 	else
-	{
 		printf("[%i] %s\n", h->len, h->str);
-	}
 
 	return (index);
 
