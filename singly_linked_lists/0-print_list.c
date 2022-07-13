09e632c42591cfdeda0a0cc3cf355e6811a0e09d
Rbrogniez <5013@holbertonschool.com>
2022-07-13 13:47:24 +0200

    fix betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
+ * print_list - function that prints all the elements of a struct list
+ * @h: a pointer to a struct
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
@@ -14,14 +15,15 @@ size_t print_list(const list_t *h)
 
 		if (h->str == NULL)
 		{
-			printf ("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 
 		else
 	{
-		printf ("[%i] %s\n", h->len, h->str );
+		printf("[%i] %s\n", h->len, h->str);
 	}
 
 	return (index);
 
 }
+
