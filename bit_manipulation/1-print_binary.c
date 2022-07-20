99db69f2548f5aeb1c1ff8f6ce0ef347cf38cbd9
Rbrogniez <5013@holbertonschool.com>
2022-07-20 10:39:04 +0200

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -2,14 +2,13 @@
 
 /**
  * print_binary - prints the binary representation of a number
+ *
  * @n: The number
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
-
 		print_binary(n >> 1);
-
 	_putchar((n & 1) + '0');
 }
