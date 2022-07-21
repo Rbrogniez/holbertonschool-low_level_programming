c1cab6b958f9a03cd2d7e0cfd9a09ae1a835c10c
Rbrogniez <5013@holbertonschool.com>
2022-07-21 14:08:43 +0200

    task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * @brief
+ *
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+
+{
+	size_t i = 0;
+
+	while (h != NULL)
+
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+
+	return(i);
+}
+
+
