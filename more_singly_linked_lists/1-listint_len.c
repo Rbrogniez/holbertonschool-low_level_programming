c26a8a0974a1f4a4eb0281cd8705d87a248273fe
Rbrogniez <5013@holbertonschool.com>
2022-07-21 10:50:10 +0200

    task 1

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -9,13 +9,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	int i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		h = h->next;
-		count++;
+		i++;
 	}
 
-	return (count);
+	return (i);
 }
